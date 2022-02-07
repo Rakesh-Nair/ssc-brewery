dde327e29e04e84d6c67a7f0358becb7df7e7985
rakesh-nair <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>
2022-02-07 18:12:36 +0530

    Add user configuration in Security Config

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -25,5 +32,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.formLogin().and()
 		.httpBasic();
 	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails admin = User
+				.builder()
+				.username("admin")
+				.password("admin")
+				.roles("ADMIN")
+				.build();		
+		UserDetails user = User
+				.builder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	
 
 }
