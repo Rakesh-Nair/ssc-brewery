ad4270b8d44362c5f52446f554a2ce9884fa85c6
rakesh-nair <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>
2022-02-08 16:07:35 +0530

    Same function using AuthenticationManagerBuilder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,28 +35,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
-		UserDetails admin = User
-				.builder()
-				.username("admin")
-				.password("admin")
-				.roles("ADMIN")
-				.build();		
-		UserDetails user = User
-				.builder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		
-		return new InMemoryUserDetailsManager(admin, user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin")
+		.password("{noop}admin")
+		.roles("ADMIN")
+		.and()
+		.withUser("user")
+		.password("{noop}password")
+		.roles("USER");
 	}
+
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+	/*
+	 * @Override
+	 * 
+	 * @Bean protected UserDetailsService userDetailsService() { UserDetails admin =
+	 * User .builder() .username("admin") .password("admin") .roles("ADMIN")
+	 * .build(); UserDetails user = User .builder() .username("user")
+	 * .password("password") .roles("USER") .build();
+	 * 
+	 * return new InMemoryUserDetailsManager(admin, user); }
+	 * 
+	 * @Bean public PasswordEncoder passwordEncoder() { return
+	 * NoOpPasswordEncoder.getInstance(); }
+	 */
 	
 	
 
