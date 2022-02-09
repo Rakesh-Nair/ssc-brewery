6b307730e41d5fbc230b36ab689aee9ffa423df0
rakesh-nair <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>
2022-02-09 19:41:13 +0530

    Adding custom filter

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,20 +3,34 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import guru.sfg.brewery.SfgPasswordEncoderFactories;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
+		RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+		filter.setAuthenticationManager(authenticationManager);
+		return filter;
+	}
+	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
+				UsernamePasswordAuthenticationFilter.class)
+		.csrf().disable();
 		http
 		.authorizeRequests(authorize ->{
 			authorize.antMatchers("/","/webjars/**","/login","/resources/**").permitAll();
